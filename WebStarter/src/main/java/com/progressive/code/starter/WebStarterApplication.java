78e855552dff9757a463eca20afdb8c6bea8d0b7
Alexander Braun <abraun@braun-it.ca>
2017-11-13 00:45:42 -0800

    Initial version of WebStarter project

---
+++ WebStarter/src/main/java/com/progressive/code/starter/WebStarterApplication.java
@@ -0,0 +1,12 @@
+package com.progressive.code.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStarterApplication.class, args);
+	}
+}
