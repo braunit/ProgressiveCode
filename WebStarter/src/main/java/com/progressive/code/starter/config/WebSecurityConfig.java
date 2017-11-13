e887740df8fd0d5df452eecf13d720e3532a7f89
Alexander Braun <abraun@braun-it.ca>
2017-11-13 00:51:06 -0800

    added Logout functionality

--- WebStarter/src/main/java/com/progressive/code/starter/config/WebSecurityConfig.java
+++ WebStarter/src/main/java/com/progressive/code/starter/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest()
 				.permitAll()
 			.and()
-				.formLogin().loginPage("/login").permitAll();
+				.formLogin().loginPage("/login").permitAll()
+			.and()
+				.logout()
+			       .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			       .permitAll();
 	}
 	
     @Autowired
