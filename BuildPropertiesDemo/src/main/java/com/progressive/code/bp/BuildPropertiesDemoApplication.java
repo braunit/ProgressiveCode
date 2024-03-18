fad30c3bd65ce8601f90c87b8e85ca7c518e0e40
Alexander Braun <abraun@braun-it.ca>
2024-03-18 14:47:45 -0700

    Added example for accessing build properties at run time

---
+++ BuildPropertiesDemo/src/main/java/com/progressive/code/bp/BuildPropertiesDemoApplication.java
@@ -0,0 +1,29 @@
+package com.progressive.code.bp;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.progressive.code.bp"})
+public class BuildPropertiesDemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private BuildProperties buildProperties;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildPropertiesDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(String.format("Version: %s Time: %s", buildProperties.getVersion(), buildProperties.getTime()));
+		System.out.println("Current Time is: " + LocalDateTime.now());
+	}
+
+}
---
+++ BuildPropertiesDemo/src/test/java/com/progressive/code/bp/BuildPropertiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressive.code.bp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildPropertiesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
